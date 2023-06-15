33c7e649f9f3c30a58cb3b6896051765bdb5c70c
Navellia <ncent4lyfe@gmail.com>
2023-06-15 15:54:13 +0700

    money
    money detection

--- android/app/src/main/java/com/example/reachyou/ui/screen/scannerBISINDO/CameraActivity.kt
+++ android/app/src/main/java/com/example/reachyou/ui/screen/scannerBISINDO/CameraActivity.kt
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.reachyou.R
 
-class CameraActivity : AppCompatActivity() {
+class CameraActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
