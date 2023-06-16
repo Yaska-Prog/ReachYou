524c75cf128145dc3c5b63e6eeec27786cbe0e05
Anri20 <88647934+Anri20@users.noreply.github.com>
2023-06-16 12:23:15 +0700

    add new model

--- android/app/src/main/java/com/example/reachyou/ui/screen/scannerBISINDO/DetectorActivityBisindo.kt
+++ android/app/src/main/java/com/example/reachyou/ui/screen/scannerBISINDO/DetectorActivityBisindo.kt
@@ -203,7 +203,7 @@ class DetectorActivityBisindo : CameraActivityBisindo(), OnImageAvailableListene
         // Configuration values for the prepackaged SSD model.
         private val TF_OD_API_INPUT_SIZE = 640
         private val TF_OD_API_IS_QUANTIZED = true
-        private val TF_OD_API_MODEL_FILE = "Bisindo.tflite"
+        private val TF_OD_API_MODEL_FILE = "Bisindo1.tflite"
         private val TF_OD_API_LABELS_FILE = "file:///android_asset/labelHuruf.txt"
         private val MODE = DetectorMode.TF_OD_API
         // Minimum detection confidence to track a detection.
